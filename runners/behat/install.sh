9f09db6244705157ea998dd2e43ba83cf558c101
Fabian Franz <github@fabian-franz.de>
2014-12-12 06:55:19 +0100

    debug

--- runners/behat/install.sh
+++ runners/behat/install.sh
@@ -15,5 +15,6 @@ drupal_ti_ensure_php_for_drush_webserver
 drupal_ti_ensure_drush
 
 # Install firefox
-drupal_ti_apt_get update >/dev/null
-drupal_ti_apt_get install -y firefox
+
+#drupal_ti_apt_get update >/dev/null
+#drupal_ti_apt_get install -y firefox
